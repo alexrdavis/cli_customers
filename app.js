22316aa73d44b8ed151e9f7129f9346c1e120f84
Alex <alexandradavis13@gmail.com>
2022-08-02 15:21:10 -0700

    Delete operation functional

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import inquirer from 'inquirer';
 import { readAllCustomers, readCustomerById } from "../cli_customers/operations/read.js"
+import { deleteCustomer } from "../cli_customers/operations/delete.js"
 
 inquirer
   .prompt([
@@ -19,9 +20,15 @@ inquirer
   .then((answer) => {
     // if read operation, return read by id
     if(answer.operation == "Read") {
-      console.log(readCustomerById(1))
+      // new prompt to get ID for customer 
+      inquirer.prompt([{name: "getId", type: "input", message:"Enter customer ID"}])
+      .then(response => {console.log(readCustomerById(response.getId))})
     // if read all operation, return all customers
     } else if(answer.operation == "Read All") {
       readAllCustomers()
+    } else if(answer.operation == "Delete") {
+      // new prompt to get ID for customer to remove
+      inquirer.prompt([{name: "getId", type: "input", message:"Enter customer ID"}])
+      .then(response => {console.log(deleteCustomer(response.getId))})
     }
   });
---
+++ operations/delete.js
@@ -0,0 +1,23 @@
+import fs from "fs"
+import { importCustomers } from "../data/import_data.js"
+
+let customers = importCustomers("../cli_customers/data/customers.txt")
+
+export function deleteCustomer(id) {
+   // set current customers to an array
+   let arr = customers;
+   // empty file
+   fs.writeFile("../cli_customers/data/customers.txt", "", function(){})
+    // loop through customers
+    for(let i = 0; i < arr.length; i++) {
+        // check for match
+        if(arr[i].id == id) {
+            // remove from array
+            let index = arr.indexOf(arr[i])
+            arr.splice(index, 1)
+        }
+        // append to file with the array of the removed customer
+        fs.appendFile("../cli_customers/data/customers.txt", JSON.stringify(arr[i])+"\n", function(){})
+    }
+    return arr
+}
